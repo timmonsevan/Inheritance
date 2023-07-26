fc8a0ab1dee947d57165a60b9070ec8fa89150e6
timmonsevan <timmonsevan@gmail.com>
2023-07-26 15:00:26 -0400

    testing the vehicle class in main() and it works

--- Inheritance/Source.cpp
+++ Inheritance/Source.cpp
@@ -1,15 +1,29 @@
 // Evan Timmons
 // CIS 1202 5T1
 // 7/26/23
-
 #include<iostream>
+#include<string>
+#include"Vehicle.h"
+#include"Car.h"
+#include"Truck.h"
 using namespace std;
 
 
 
 int main()
 {
+	string companyName;
+	int yearBuilt;
 
+	cout << "Enter the manufacturer: " << endl;
+	getline(cin, companyName);
+	cout << "Enter the year built: " << endl;
+	cin >> yearBuilt;
+	
+	Vehicle vehicle1;
+	vehicle1.setManufacturer(companyName);
+	vehicle1.setYear(yearBuilt);
+	vehicle1.displayInfo();
 
 
 
